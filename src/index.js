eda2981f3e13fc237ad221010f32ea53a1c3ff4d
Henrik <henrikolsson@outlook.com>
2019-03-12 09:17:43 +0100

    Fix lint issues

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require('express')
 
-const ALLOWED_HTTP_METHODS = ["GET", "POST", "PUT", "DELETE", "PATCH"]
+const ALLOWED_HTTP_METHODS = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
 
 /**
  * @typedef {Object} Endpoint
@@ -18,74 +18,74 @@ const ALLOWED_HTTP_METHODS = ["GET", "POST", "PUT", "DELETE", "PATCH"]
  */
 
 /**
- * @example 
+ * @example
  * // Returns { "CLIENT": express.Router }
  * .parse({...}, ["CLIENT"], {...})
- * 
+ *
  * @param {RestApiJson} restApiJson The restAPI part of the serviceDescription
  * @param {string[]} allowedTypes The allowed types of paths
  * @param {Object} routeHandler A object containing all of the handlers for the routes
  * @return {Object.<string, express.Router>} Object containing a router for each allowed type
  */
-function parse(restApiJson, allowedTypes, routeHandlers) {
-    /** @type {Object.<string, express.Router>} */
-    const routers = {};
-    for (const allowedType of allowedTypes) {
-        routers[allowedType] = express.Router();
+function parse (restApiJson, allowedTypes, routeHandlers) {
+  /** @type {Object.<string, express.Router>} */
+  const routers = {}
+  for (const allowedType of allowedTypes) {
+    routers[allowedType] = express.Router()
+  }
+
+  for (var key in restApiJson.endpoints) {
+    if (!restApiJson.endpoints.hasOwnProperty(key)) {
+      continue
     }
 
-    for (var key in restApiJson.endpoints) {
-        if (!restApiJson.endpoints.hasOwnProperty(key)) {
-            continue;
-        }
+    const methods = restApiJson.endpoints[key]
+    for (const method of methods) {
+      // TODO: Since we have method.parameters we could do some nice things like check that is exists in the request.
+      if (!routers.hasOwnProperty(method.type)) {
+        console.warn('Type not valid for endpoint with method ' + method.method + ' and URI ' + restApiJson.pathPrefix + key)
+        continue
+      }
 
-        const methods = restApiJson.endpoints[key]
-        for (const method of methods) {
-            // TODO: Since we have method.parameters we could do some nice things like check that is exists in the request.
-            if (!routers.hasOwnProperty(method.type)) {
-                console.warn("Type not valid for endpoint with method " + method.method + " and URI " + restApiJson.pathPrefix + key);
-                continue;
-            }
+      if (!routeHandlers.hasOwnProperty(method.handlingFunction)) {
+        console.warn("Can't find handler function '" + method.handlingFunction + "' for endpoint with method " + method.method + ' and URI ' + restApiJson.pathPrefix + key)
+        continue
+      }
 
-            if (!routeHandlers.hasOwnProperty(method.handlingFunction)) {
-                console.warn("Can't find handler function '" + method.handlingFunction + "' for endpoint with method " + method.method + " and URI " + restApiJson.pathPrefix + key);
-                continue;
-            }
-
-            const router = routers[method.type];
-            if (ALLOWED_HTTP_METHODS.includes(method.method)) {
-                // Express has all HTTP methods/verbs as functions in lowercase
-                router[method.method.toLowerCase()](
-                    restApiJson.pathPrefix + key,
-                    [parameterCheckHandler(method.parameters), routeHandlers[method.handlingFunction]]
-                );
-            } else {
-                console.warn("Method not allowed for endpoint with method " + method.method + " and URI " + restApiJson.pathPrefix + key);
-            }
-        }
+      const router = routers[method.type]
+      if (ALLOWED_HTTP_METHODS.includes(method.method)) {
+        // Express has all HTTP methods/verbs as functions in lowercase
+        router[method.method.toLowerCase()](
+          restApiJson.pathPrefix + key,
+          [parameterCheckHandler(method.parameters), routeHandlers[method.handlingFunction]]
+        )
+      } else {
+        console.warn('Method not allowed for endpoint with method ' + method.method + ' and URI ' + restApiJson.pathPrefix + key)
+      }
     }
+  }
 
-    return routers;
+  return routers
 }
 
 /**
- * Creates a function for 
- * 
+ * Creates a function for
+ *
  * @param {Object} params - The params to check for
  */
-function parameterCheckHandler(params) {
-    return (req, res, next) => {
-        if (typeof params === 'object') {
-            for (const param of Object.keys(params)) {
-                const requestParams = req.method === 'GET' ? req.query : req.body;
-                if (!requestParams[param]) {
-                    res.send(500);
-                    return
-                }
-            }
+function parameterCheckHandler (params) {
+  return (req, res, next) => {
+    if (typeof params === 'object') {
+      for (const param of Object.keys(params)) {
+        const requestParams = req.method === 'GET' ? req.query : req.body
+        if (!requestParams[param]) {
+          res.send(500)
+          return
         }
-        next();
-    };
+      }
+    }
+    next()
+  }
 }
 
-module.exports.parse = parse;
+module.exports.parse = parse
--- test/test.js
+++ test/test.js
@@ -1,242 +1,241 @@
-const express = require('express');
-const expressRouterMSDescription = require('../src/index.js');
-require('mocha-sinon');
+const express = require('express')
+const expressRouterMSDescription = require('../src/index.js')
+require('mocha-sinon')
 
-const request = require('supertest');
+const request = require('supertest')
 
-const ALLOWED_TYPES = ["CLIENT", "ADMIN", "EXTERNAL"];
+const ALLOWED_TYPES = ['CLIENT', 'ADMIN', 'EXTERNAL']
 
-var assert = require('assert');
+var assert = require('assert')
 describe('ExpressRouterMSDescription', function () {
+  function createExpress () {
+    const app = express()
+    app.use(express.json())
+    return app
+  }
 
-    function createExpress() {
-        const app = express();
-        app.use(express.json());
-        return app;
-    }
+  beforeEach(function () {
+    this.sinon.stub(console, 'warn')
+  })
 
-    beforeEach(function () {
-        this.sinon.stub(console, 'warn');
-    });
-
-    describe('One router (CLIENT POST)', function () {
-        const app = createExpress();
-        const routers = expressRouterMSDescription.parse(
+  describe('One router (CLIENT POST)', function () {
+    const app = createExpress()
+    const routers = expressRouterMSDescription.parse(
+      {
+        pathPrefix: '/authenticate/bankid',
+        endpoints: {
+          '/': [
             {
-                pathPrefix: "/authenticate/bankid",
-                endpoints: {
-                    "/": [
-                        {
-                            method: "POST",
-                            type: "CLIENT",
-                            handlingFunction: "handleAuthenticate",
-                            description: "Checks the SSID and then starts an authentication",
-                            parameters: {
-                                ssid: "string with Swedish social security number"
-                            }
-                        },
-                        {
-                            method: "GET",
-                            type: "CLIENT",
-                            handlingFunction: "getAuthentication",
-                            description: "Gets something",
-                            parameters: {
-                                test: "some test param"
-                            }
-                        }
-                    ]
-                }
+              method: 'POST',
+              type: 'CLIENT',
+              handlingFunction: 'handleAuthenticate',
+              description: 'Checks the SSID and then starts an authentication',
+              parameters: {
+                ssid: 'string with Swedish social security number'
+              }
             },
-            ALLOWED_TYPES,
             {
-                handleAuthenticate: (req, res, next) => res.sendStatus(200),
-                getAuthentication: (req, res, next) => res.sendStatus(200)
+              method: 'GET',
+              type: 'CLIENT',
+              handlingFunction: 'getAuthentication',
+              description: 'Gets something',
+              parameters: {
+                test: 'some test param'
+              }
             }
-        );
+          ]
+        }
+      },
+      ALLOWED_TYPES,
+      {
+        handleAuthenticate: (req, res, next) => res.sendStatus(200),
+        getAuthentication: (req, res, next) => res.sendStatus(200)
+      }
+    )
 
-        app.use('/', routers.CLIENT);
+    app.use('/', routers.CLIENT)
 
-        it('respond with 200', function (done) {
-            request(app)
-                .post('/authenticate/bankid/')
-                .send({ ssid: "testSSID" })
-                .expect(200, done);
-        });
+    it('respond with 200', function (done) {
+      request(app)
+        .post('/authenticate/bankid/')
+        .send({ ssid: 'testSSID' })
+        .expect(200, done)
+    })
 
-        it('respond with 200, correct params', function (done) {
-            request(app)
-                .get('/authenticate/bankid?test=hello')
-                .expect(200, done);
-        });
+    it('respond with 200, correct params', function (done) {
+      request(app)
+        .get('/authenticate/bankid?test=hello')
+        .expect(200, done)
+    })
 
-        it('respond with 200, incorrect params', function (done) {
-            request(app)
-                .post('/authenticate/bankid/')
-                .send({ test: "hello" })
-                .expect(500, done);
-        });
+    it('respond with 200, incorrect params', function (done) {
+      request(app)
+        .post('/authenticate/bankid/')
+        .send({ test: 'hello' })
+        .expect(500, done)
+    })
 
-        it('respond with 404, wrong method', function (done) {
-            request(app)
-                .put('/authenticate/bankid/')
-                .expect(404, done);
-        });
+    it('respond with 404, wrong method', function (done) {
+      request(app)
+        .put('/authenticate/bankid/')
+        .expect(404, done)
+    })
 
-        it('respond with 404, wrong URL', function (done) {
-            request(app)
-                .get('/authenticate/bankid/test')
-                .expect(404, done);
-        });
-    });
+    it('respond with 404, wrong URL', function (done) {
+      request(app)
+        .get('/authenticate/bankid/test')
+        .expect(404, done)
+    })
+  })
 
-    describe('Multiple routers', function () {
-        const app = createExpress();
-        const routers = expressRouterMSDescription.parse(
+  describe('Multiple routers', function () {
+    const app = createExpress()
+    const routers = expressRouterMSDescription.parse(
+      {
+        pathPrefix: '/authenticate/bankid',
+        endpoints: {
+          '/': [
             {
-                pathPrefix: "/authenticate/bankid",
-                endpoints: {
-                    "/": [
-                        {
-                            method: "POST",
-                            type: "CLIENT",
-                            handlingFunction: "handleAuthenticate",
-                            description: "Checks the SSID and then starts an authentication",
-                            parameters: {
-                                ssid: "string with Swedish social security number"
-                            }
-                        },
-                        {
-                            method: "GET",
-                            type: "ADMIN",
-                            handlingFunction: "handleListAuthentications",
-                            description: "Lists all auth which have been made"
-                        }
-                    ]
-                }
+              method: 'POST',
+              type: 'CLIENT',
+              handlingFunction: 'handleAuthenticate',
+              description: 'Checks the SSID and then starts an authentication',
+              parameters: {
+                ssid: 'string with Swedish social security number'
+              }
             },
-            ALLOWED_TYPES,
             {
-                handleAuthenticate: (req, res, next) => res.sendStatus(200),
-                handleListAuthentications: (req, res, next) => res.sendStatus(200),
+              method: 'GET',
+              type: 'ADMIN',
+              handlingFunction: 'handleListAuthentications',
+              description: 'Lists all auth which have been made'
             }
-        );
+          ]
+        }
+      },
+      ALLOWED_TYPES,
+      {
+        handleAuthenticate: (req, res, next) => res.sendStatus(200),
+        handleListAuthentications: (req, res, next) => res.sendStatus(200)
+      }
+    )
 
-        app.use("/client", routers.CLIENT);
-        app.use("/admin", routers.ADMIN);
+    app.use('/client', routers.CLIENT)
+    app.use('/admin', routers.ADMIN)
 
-        it('respond with 200 CLIENT', function (done) {
-            request(app)
-                .post('/client/authenticate/bankid/')
-                .send({ ssid: "testSSID" })
-                .expect(200, done);
-        });
+    it('respond with 200 CLIENT', function (done) {
+      request(app)
+        .post('/client/authenticate/bankid/')
+        .send({ ssid: 'testSSID' })
+        .expect(200, done)
+    })
 
-        it('respond with 200 ADMIN', function (done) {
-            request(app)
-                .get('/admin/authenticate/bankid/')
-                .expect(200, done);
-        });
+    it('respond with 200 ADMIN', function (done) {
+      request(app)
+        .get('/admin/authenticate/bankid/')
+        .expect(200, done)
+    })
 
-        it('respond with 404, wrong method/URL', function (done) {
-            request(app)
-                .post('/admin/authenticate/bankid/')
-                .expect(404, done);
-        });
-    });
+    it('respond with 404, wrong method/URL', function (done) {
+      request(app)
+        .post('/admin/authenticate/bankid/')
+        .expect(404, done)
+    })
+  })
 
-    describe('Incorrect setups', function () {
-        it('failed to add routes, no matching method', function (done) {
-            const app = createExpress();
-            const routers = expressRouterMSDescription.parse(
-                {
-                    pathPrefix: "/authenticate/bankid",
-                    endpoints: {
-                        "/": [
-                            {
-                                method: "POST",
-                                type: "CLIENT",
-                                handlingFunction: "handleAuthenticate",
-                                description: "Checks the SSID and then starts an authentication",
-                                parameters: {
-                                    ssid: "string with Swedish social security number"
-                                }
-                            }
-                        ]
-                    }
-                },
-                ALLOWED_TYPES,
-                {}
-            );
-            assert(console.warn.calledWith("Can't find handler function 'handleAuthenticate' for endpoint with method POST and URI /authenticate/bankid/"))
-
-            app.use("/", routers.CLIENT);
-            request(app)
-                .post('/admin/authenticate/bankid/')
-                .expect(404, done);
-        });
-
-        it('fail to add route, method not allowed', function (done) {
-            const app = createExpress();
-            const routers = expressRouterMSDescription.parse(
-                {
-                    pathPrefix: "/authenticate/bankid",
-                    endpoints: {
-                        "/": [
-                            {
-                                method: "HEAD",
-                                type: "CLIENT",
-                                handlingFunction: "handleAuthenticate",
-                                description: "Checks the SSID and then starts an authentication",
-                                parameters: {
-                                    ssid: "string with Swedish social security number"
-                                }
-                            }
-                        ]
-                    }
-                },
-                ALLOWED_TYPES,
-                {
-                    handleAuthenticate: (req, res, next) => res.sendStatus(200),
+  describe('Incorrect setups', function () {
+    it('failed to add routes, no matching method', function (done) {
+      const app = createExpress()
+      const routers = expressRouterMSDescription.parse(
+        {
+          pathPrefix: '/authenticate/bankid',
+          endpoints: {
+            '/': [
+              {
+                method: 'POST',
+                type: 'CLIENT',
+                handlingFunction: 'handleAuthenticate',
+                description: 'Checks the SSID and then starts an authentication',
+                parameters: {
+                  ssid: 'string with Swedish social security number'
                 }
-            );
-            assert(console.warn.calledWith("Method not allowed for endpoint with method HEAD and URI /authenticate/bankid/"))
+              }
+            ]
+          }
+        },
+        ALLOWED_TYPES,
+        {}
+      )
+      assert(console.warn.calledWith("Can't find handler function 'handleAuthenticate' for endpoint with method POST and URI /authenticate/bankid/"))
 
-            app.use("/", routers.CLIENT);
-            request(app)
-                .post('/admin/authenticate/bankid/')
-                .expect(404, done);
-        });
+      app.use('/', routers.CLIENT)
+      request(app)
+        .post('/admin/authenticate/bankid/')
+        .expect(404, done)
+    })
 
-        it('fail to add route, type not valid', function (done) {
-            const app = createExpress();
-            const routers = expressRouterMSDescription.parse(
-                {
-                    pathPrefix: "/authenticate/bankid",
-                    endpoints: {
-                        "/": [
-                            {
-                                method: "POST",
-                                type: "BACKEND",
-                                handlingFunction: "handleAuthenticate",
-                                description: "Checks the SSID and then starts an authentication",
-                                parameters: {
-                                    ssid: "string with Swedish social security number"
-                                }
-                            }
-                        ]
-                    }
-                },
-                ALLOWED_TYPES,
-                {
-                    handleAuthenticate: (req, res, next) => res.sendStatus(200),
+    it('fail to add route, method not allowed', function (done) {
+      const app = createExpress()
+      const routers = expressRouterMSDescription.parse(
+        {
+          pathPrefix: '/authenticate/bankid',
+          endpoints: {
+            '/': [
+              {
+                method: 'HEAD',
+                type: 'CLIENT',
+                handlingFunction: 'handleAuthenticate',
+                description: 'Checks the SSID and then starts an authentication',
+                parameters: {
+                  ssid: 'string with Swedish social security number'
                 }
-            );
-            assert(console.warn.calledWith("Type not valid for endpoint with method POST and URI /authenticate/bankid/"))
+              }
+            ]
+          }
+        },
+        ALLOWED_TYPES,
+        {
+          handleAuthenticate: (req, res, next) => res.sendStatus(200)
+        }
+      )
+      assert(console.warn.calledWith('Method not allowed for endpoint with method HEAD and URI /authenticate/bankid/'))
 
-            app.use("/", routers.CLIENT);
-            request(app)
-                .post('/admin/authenticate/bankid/')
-                .expect(404, done);
-        });
-    });
-});
+      app.use('/', routers.CLIENT)
+      request(app)
+        .post('/admin/authenticate/bankid/')
+        .expect(404, done)
+    })
+
+    it('fail to add route, type not valid', function (done) {
+      const app = createExpress()
+      const routers = expressRouterMSDescription.parse(
+        {
+          pathPrefix: '/authenticate/bankid',
+          endpoints: {
+            '/': [
+              {
+                method: 'POST',
+                type: 'BACKEND',
+                handlingFunction: 'handleAuthenticate',
+                description: 'Checks the SSID and then starts an authentication',
+                parameters: {
+                  ssid: 'string with Swedish social security number'
+                }
+              }
+            ]
+          }
+        },
+        ALLOWED_TYPES,
+        {
+          handleAuthenticate: (req, res, next) => res.sendStatus(200)
+        }
+      )
+      assert(console.warn.calledWith('Type not valid for endpoint with method POST and URI /authenticate/bankid/'))
+
+      app.use('/', routers.CLIENT)
+      request(app)
+        .post('/admin/authenticate/bankid/')
+        .expect(404, done)
+    })
+  })
+})
